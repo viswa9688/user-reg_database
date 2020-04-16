ec89b74c42bed913f64e37eff90191d5ab33412d
Hi <viswa968@msitprogram.net>
2020-04-17 01:56:55 +0530

    validation_done

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template
 
+from wtform_fields import *
+
 #standard flask instruction set by python community to call Flask 
 
 app=Flask(__name__)
@@ -7,9 +9,16 @@ app=Flask(__name__)
 
 app.secret_key='replace later'
 
-@app.route("/",methods=['GET','POST'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+
+    reg_form = RegistrationForm()
+
+    # Update database if validation success
+    if reg_form.validate_on_submit():
+        return "Great Success"
+
+    return render_template("index.html", form=reg_form)
 
 if __name__ == "__main__":
     app.run(debug=True)
